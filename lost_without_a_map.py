ed7bf15ce0c2358ecf29f7d740991578b01763a2
Ziaullah Khan <ziaullahkhan.se@gmail.com>
2026-08-28 20:51:29 +0500

    Update lost_without_a_map.py

--- lost_without_a_map.py
+++ lost_without_a_map.py
@@ -6,7 +6,7 @@ def maps(a):
     
     return result
 
-# Test
+
 print(maps([1, 2, 3]))    
-print(maps([4, 5, 6]))    # [8, 10, 12]
-print(maps([-1, 0, 1]))   # [-2, 0, 2]
+print(maps([4, 5, 6]))  
+print(maps([-1, 0, 1]))   
